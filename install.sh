04dbe0419b30bf7a67cde3e87512b48c1d87ea65
itznokx <dimitri.b.medeiros@gmail.com>
2025-09-01 14:53:50 -0300

    fix distro localizer function in install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-echo "Dependencies Installer 0.0.1"
-
+echo "Nokx dot files configurer 0.0.1"
 echo "verifying distro"
 distro_id="$(cat /etc/*-release | grep "ID=")"
-echo "${distro}"
-
+echo "${distro_id}"
+debian_based_distros=(
+    "ubuntu"
+    "linuxmint"
+    "pop"
+    "kali"
+    "elementary"
+)
