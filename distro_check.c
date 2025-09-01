765547a9b80ec2fb826217ef4e2d2015e0bd8747
itznokx <dimitri.b.medeiros@gmail.com>
2025-09-01 15:39:34 -0300

    distro checker script in C fixed

--- distro_check.c
+++ distro_check.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main (int narg,char* argv[]){
-    printf("Distro: %s",argv[2]);
+    size_t x1 = strcspn(argv[2],"=");
+    char distro_name[strlen(argv[2])-x1];
+    strncpy(distro_name, argv[2]+x1+1,strlen(argv[2])-x1);
+    printf("Distro: %s",distro_name);
+
     return 0;
 }
