f571128fd4b94b18bb45c98d093eaed84aa48f67
itznokx <dimitri.b.medeiros@gmail.com>
2025-09-01 15:12:41 -0300

    fix bash script to check distro

--- install.sh
+++ install.sh
@@ -3,11 +3,4 @@
 echo "Nokx dot files configurer 0.0.1"
 echo "verifying distro"
 distro_id="$(cat /etc/*-release | grep "ID=")"
-echo "${distro_id}"
-debian_based_distros=(
-    "ubuntu"
-    "linuxmint"
-    "pop"
-    "kali"
-    "elementary"
-)
+echo "$(./distro_check $distro_id)"
