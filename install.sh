13e75c6459052feb4a4e877a969aba1324debc24
itznokx <dimitri.b.medeiros@gmail.com>
2025-09-02 10:49:39 -0300

    generic nvim install function in install.sh file

--- install.sh
+++ install.sh
@@ -4,6 +4,15 @@ echo "Nokx dot files configurer 0.0.1"
 echo "verifying distro"
 distro_id="$(cat /etc/*-release | grep "ID=")"
 script_base="$(./distro_check $distro_id)"
+
+nvim_install() { #all distro linux
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+    sudo rm -rf /opt/nvim # clean install (removable)
+    sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz # extract
+    sudo mv /opt/nvim-linux-x86_64 /opt/nvim
+    sudo ln -s -f /opt/nvim/bin/nvim /bin/nvim
+}
+
 # DEBIAN SCRIPT
 if [[ "$script_base" = "debian" ]]; then
     echo "debian-based distro identified"
@@ -21,6 +30,11 @@ if [[ "$script_base" = "debian" ]]; then
     echo "install curl"
     sudo apt-get install curl
     curl --version
+    echo "installing zip & unzip tools"
+    sudo apt install zip
+    sudo apt install unzip
+    sudo apt install tar
+    sudo apt install dpkg
     :'
     echo "installing nvim"
     curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
@@ -34,7 +48,6 @@ if [[ "$script_base" = "debian" ]]; then
     echo "copying packer neovim repo"
     git clone --depth 1 https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
     '
-    
 elif [[ "$script_base" = "arch" ]]; then
     echo "arch-based distro identified"
 elif [[ "$script_base" = "invalid distro" ]]; then
